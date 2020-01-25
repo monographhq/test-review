8daff4ecd0ec98942dd4a0d241b410ddb099fc96
Moe Amaya <moeamaya@gmail.com>
2020-01-25 10:42:03 -0800

    Updates

---
+++ precache-manifest.13a7b71f94805947dbbd5c73baf151ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9d5578cceb9e42eb2f5cb4ea245b34",
+    "url": "/test-review/index.html"
+  },
+  {
+    "revision": "ff6453c18c0536f3c5c0",
+    "url": "/test-review/static/css/main.51a76c01.chunk.css"
+  },
+  {
+    "revision": "d5eb51d64392ac9b174e",
+    "url": "/test-review/static/js/2.8bea91a9.chunk.js"
+  },
+  {
+    "revision": "ff6453c18c0536f3c5c0",
+    "url": "/test-review/static/js/main.6b424f5a.chunk.js"
+  },
+  {
+    "revision": "98200f5bf044fe5de5ba",
+    "url": "/test-review/static/js/runtime-main.5655870f.js"
+  },
+  {
+    "revision": "baaa27d08142d4550d8777cf098bc1c2",
+    "url": "/test-review/static/media/logo.baaa27d0.png"
+  }
+]);
